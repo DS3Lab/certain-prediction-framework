95846820243e63b27837aa89cefdc325555cf6f5
Bojan Karlaš <bojan.karlas@gmail.com>
2020-05-13 00:56:58 +0200

    Enable probability mode. Fix bug with boundary condition in dynamic program.

--- cpf.py
+++ cpf.py
@@ -3,6 +3,7 @@
 import argparse
 import collections
 import csv
+import functools
 import itertools
 import math
 import re
@@ -33,6 +34,16 @@ def compute_similarities(dataset, example, kernel=DEFAULT_KERNEL):
     similarities = [[1 / (x + sys.float_info.epsilon) for x in row] for row in distances]
 
     return similarities
+
+
+def compute_probabilities(dataset_probs):
+    probabilities = []
+    for row in dataset_probs:
+        row_probabilities = []
+        for candidate_probs in itertools.product(*row):
+            row_probabilities.append(functools.reduce((lambda x, y: x * y), candidate_probs))
+        probabilities.append(row_probabilities)
+    return probabilities
 
 
 def q1(similarities, labels, k=DEFAULT_K):
@@ -67,7 +78,7 @@ def q1(similarities, labels, k=DEFAULT_K):
 
 def compute_dp(labels, alpha, i, m, c, k):
 
-    dp = [[1 for _ in range(k + 1)]]
+    dp = [[1] + [0 for _ in range(k)]]
 
     for n in range(len(alpha)):
         if labels[n] == c:
@@ -87,13 +98,20 @@ def compute_dp(labels, alpha, i, m, c, k):
     return dp[-1]
 
 
-def q2(similarities, labels, k=DEFAULT_K):
+def q2(similarities, labels, k=DEFAULT_K, probabilities=None):
 
     # Initialize helper variables.
     classes = sorted(list(set(labels)))
     answer = dict((c, 0) for c in classes)
     m = [len(row) for row in similarities]
     alpha = [0 for _ in range(len(similarities))]
+
+    # If probabilities are provided, we are in probability mode and need to
+    # assert that a probability distribution is defined over all row candidates.
+    prob_mode = probabilities is not None
+    if prob_mode:
+        assert(all(len(similarities[i]) == len(probabilities[i]) for i in range(len(similarities))))
+        m = [1.0 for _ in similarities]
 
     # Compute the sorting order of similarity candidates.
     similarities = [[(i, j, s) for (j, s) in enumerate(row)] for (i, row) in enumerate(similarities)]
@@ -103,7 +121,10 @@ def q2(similarities, labels, k=DEFAULT_K):
     for i, j, _ in sorted_similarities:
 
         # Maintain the similarity tally.
-        alpha[i] += 1
+        if prob_mode:
+            alpha[i] += probabilities[i][j]
+        else:
+            alpha[i] += 1
 
         # Compute the dynamic program for all classes.
         dp = {}
@@ -120,6 +141,11 @@ def q2(similarities, labels, k=DEFAULT_K):
             support = 1
             for idx, c in enumerate(classes):
                 support *= dp[c][gamma[idx]]
+            
+            # If we are in probability mode, then we must multiply with the probability of the
+            # i-th row taking the value of the j-th candidate.
+            if prob_mode:
+                support *= probabilities[i][j]
 
             # Add the computed support to the winning class label.
             answer[winner] += support
@@ -157,12 +183,17 @@ if __name__ == "__main__":
     parser.add_argument("--no-header", action="store_true",
                         help="Assume that the training CSV file does not have a column header in the first line.")
 
+    parser.add_argument("--prob-mode", action="store_true",
+                        help="Used for the counting query (q2) to compute probabilities instead of counts.")
+
     args = parser.parse_args()
 
     # Parse the dataset CSV files.
     dataset = []
+    dataset_probs = []
     labels = []
     dataset_col_domains = []
+    dataset_col_probs = []
     with open(args.dataset, newline="") as f:
         reader = csv.reader(f)
 
@@ -172,13 +203,20 @@ if __name__ == "__main__":
             for col in header:
                 d = re.search(r"\[(.+?)\]", col)
                 if d:
-                    dataset_col_domains.append([float(x) for x in d.group(1).split("|")])
+                    domain_elements = [x.split("~") for x in d.group(1).split("|")]
+                    dataset_col_domains.append([float(x[0].strip()) for x in domain_elements])
+                    dataset_col_probs.append([float(x[1].strip()) if len(x) > 1 else 1.0 for x in domain_elements])
+
+                    # Ensure probabilities sum to 1.
+                    dataset_col_probs[-1] = [x / sum(dataset_col_probs[-1]) for x in dataset_col_probs[-1]]
                 else:
                     dataset_col_domains.append([])
+                    dataset_col_probs.append([])
 
         # Build the dataset out of cells which can contain incomplete values.
         for line in reader:
             row = []
+            row_probs = []
             for i, cell in enumerate(line):
                 if i == args.label_col:
                     labels.append(cell)
@@ -186,14 +224,22 @@ if __name__ == "__main__":
                     if cell == "":
                         assert(len(dataset_col_domains[i]) > 0)
                         row.append(dataset_col_domains[i])
+                        row_probs.append(dataset_col_probs[i])
                     else:
                         d = re.search(r"\[(.+?)\]", cell)
                         if d:
-                            row.append([float(x) for x in d.group(1).split("|")])
+                            domain_elements = [x.split("~") for x in d.group(1).split("|")]
+                            row.append([float(x[0].strip()) for x in domain_elements])
+                            row_probs.append([float(x[1].strip()) if len(x) > 1 else 1.0 for x in domain_elements])
+
+                            # Ensure probabilities sum to 1.
+                            row_probs[-1] = [x / sum(row_probs[-1]) for x in row_probs[-1]]
                         else:
                             row.append([float(cell)])
+                            row_probs.append([float(1.0)])
 
             dataset.append(row)
+            dataset_probs.append(row_probs)
 
     # Parse the example CSV file.
     with open(args.example, newline="") as f:
@@ -205,9 +251,10 @@ if __name__ == "__main__":
 
     # Answer the appropriate query.
     if args.query == "q1":
-        answer = q1(similarities, labels)
+        answer = q1(similarities, labels, k=args.k)
     else:
-        answer = q2(similarities, labels)
+        probabilities = compute_probabilities(dataset_probs) if args.prob_mode else None
+        answer = q2(similarities, labels, k=args.k, probabilities=probabilities)
 
     # Print answer.
     print("class: answer")
